d0e0f567c8141caa19de3f585843f96beeeed6dd
Mykyta <honcharov.nikita70@gmail.com>
2023-10-16 17:32:45 +0200

    first commit

---
+++ Src/WhileForLoop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class WhileForLoop {
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        //String name = "";
+        //while(name.isEmpty()){
+        //System.out.print("Enter your name :");
+        //name = scanner.nextLine();
+        //}
+        //System.out.println("Hello" + name);
+
+        //int Num = 1;
+        //for (int i = 0; i <= 100; i++) {
+        //System.out.println(Num);
+        // Num++;
+        //if (Num % 10 == 5 || Num % 10 == 2) {
+        //Num++;
+        //}
+        // if (Num / 10 % 10 == 5 || Num / 10 % 10 == 2) {
+        // Num+=10;
+        //Num++;
+
+        for(int i = 0; i<=100; i+=10){
+            System.out.println(i);
+            }
+        System.out.println("========");
+        }
+    }
+
